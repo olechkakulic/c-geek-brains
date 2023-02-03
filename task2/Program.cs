1f7a5c45dcd1450d2f12189e174934c296664c78
olechkakulic <olga.loginova@metalab.ifmo.ru>
2023-02-03 09:11:25 +0300

    add all tasks

--- task2/Program.cs
+++ task2/Program.cs
@@ -4,9 +4,9 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 Console.Write("Введите первое число: ");
-int number1 = int.Parse(Console.ReadLine());
+int number1 = int.Parse(Console.ReadLine() );
 Console.Write("Введите второе число: ");
-int number2 = int.Parse(Console.ReadLine());
+int number2 = int.Parse(Console.ReadLine() );
 if (number1>number2){
     Console.Write("max = "+number1);
 }
